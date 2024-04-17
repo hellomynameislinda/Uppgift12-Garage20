8f7cf96f973a558cc231b4f87a21d51ee9ca1af2
jarethholt <jareth.holt@gmail.com>
2024-04-17 13:26:49 +0200

    feat: Add ParkedVehicle class

---
+++ Uppgift12-Garage20/Models/ParkedVehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace Uppgift12_Garage20.Models
+{
+    public class ParkedVehicle
+    {
+        public int VehicleId { get; set; }
+        public int VehicleType { get; set; }
+        public string RegistrationNumber { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int NumberOfWheels { get; set; }
+        public DateTime ArrivalTime { get; private set; }
+    }
+}
