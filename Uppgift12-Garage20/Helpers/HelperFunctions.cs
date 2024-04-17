00be6111b57acd4007c2f21a680fe2f2d2e18b80
Linda <linda@hellomynameislinda.com>
2024-04-17 18:27:11 +0200

    Added folder helpers and file HelperFunctions.cs for functions that could have a generic use.

---
+++ Uppgift12-Garage20/Helpers/HelperFunctions.cs
@@ -0,0 +1,35 @@
+﻿namespace Uppgift12_Garage20.Helpers
+{
+    public class HelperFunctions
+    {
+        /// <summary>
+        /// Overload to make departureTime optional.
+        /// Note: Passing an arrivalTime that has not yet happened will cause
+        /// an exception to be thrown.
+        /// </summary>
+        /// <param name="arrivalTime"></param>
+        /// <returns>TimeSpan object</returns>
+        public TimeSpan ParkedTimeAmount(DateTime arrivalTime)
+        {
+            return ParkedTimeAmount(arrivalTime, DateTime.Now);
+        }
+        /// <summary>
+        /// Accepts two DateTime-objects and returns a TimeSpan-object of 
+        /// time passed between arrival and departure.
+        /// Note: Passing an arrivalTime that is later than the departureTime
+        /// will cause an exception to be thrown.
+        /// </summary>
+        /// <param name="arrivalTime"></param>
+        /// <param name="departureTime"></param>
+        /// <returns>TimeSpan object</returns>
+        public TimeSpan ParkedTimeAmount(DateTime arrivalTime, DateTime departureTime)
+        {
+            if (departureTime < arrivalTime)
+                throw new ArgumentException("Departure time cannot be before arrival time");
+
+            TimeSpan parkedTime = departureTime - arrivalTime;
+
+            return parkedTime;
+        }
+    }
+}
