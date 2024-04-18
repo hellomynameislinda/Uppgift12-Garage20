b529bf60572260e6ea7b1bad6f3a3a751a751c5a
Abed Fetrat <abedfetrat@gmail.com>
2024-04-18 15:52:20 +0200

    fix: use VehicleType Enum when instantiating ParkedVehicles for seeding

--- Uppgift12-Garage20/Data/DbInitializer.cs
+++ Uppgift12-Garage20/Data/DbInitializer.cs
@@ -4,20 +4,13 @@ namespace Uppgift12_Garage20.Data
 {
     public static class DbInitializer
     {
-        // This variable lays out the VehicleType names until the enum is created
-        private static Dictionary<int, string> VehicleTypeNames => new()
-        {
-            { 0, "Car" },
-            { 1, "Motorcycle" },
-            { 2, "Truck" },
-        };
         // Representative examples of vehicles that may be parked in the garage
         private static ParkedVehicle[] SeedVehicles => [
-            new ParkedVehicle { VehicleType = 0, RegistrationNumber = "ABC123", Color = "Beige", Make = "Toyota"       , Model = "Corolla", NumberOfWheels =  4, ArrivalTime = new DateTime(2024, 04, 17, 08, 30, 00) },
-            new ParkedVehicle { VehicleType = 0, RegistrationNumber = "DEF456", Color = "Gray" , Make = "Volvo"        , Model = "EC40"   , NumberOfWheels =  4, ArrivalTime = new DateTime(2024, 04, 17, 09, 15, 00) },
-            new ParkedVehicle { VehicleType = 1, RegistrationNumber = "XYZ098", Color = "Blue" , Make = "Yamaha"       , Model = "R7"     , NumberOfWheels =  2, ArrivalTime = new DateTime(2024, 04, 17, 09, 30, 00) },
-            new ParkedVehicle { VehicleType = 0, RegistrationNumber = "YYZ000", Color = "Red"  , Make = "Tesla"        , Model = "Model Y", NumberOfWheels =  4, ArrivalTime = new DateTime(2024, 04, 17, 09, 45, 00) },
-            new ParkedVehicle { VehicleType = 2, RegistrationNumber = "TUV765", Color = "White", Make = "Mercedes-Benz", Model = "Econic" , NumberOfWheels = 10, ArrivalTime = new DateTime(2024, 04, 17, 10, 00, 00) },
+            new ParkedVehicle { VehicleType = VehicleType.Car, RegistrationNumber = "ABC123", Color = "Beige", Make = "Toyota"       , Model = "Corolla", NumberOfWheels =  4, ArrivalTime = new DateTime(2024, 04, 17, 08, 30, 00) },
+            new ParkedVehicle { VehicleType = VehicleType.Car, RegistrationNumber = "DEF456", Color = "Gray" , Make = "Volvo"        , Model = "EC40"   , NumberOfWheels =  4, ArrivalTime = new DateTime(2024, 04, 17, 09, 15, 00) },
+            new ParkedVehicle { VehicleType = VehicleType.Motorcycle, RegistrationNumber = "XYZ098", Color = "Blue" , Make = "Yamaha"       , Model = "R7"     , NumberOfWheels =  2, ArrivalTime = new DateTime(2024, 04, 17, 09, 30, 00) },
+            new ParkedVehicle { VehicleType = VehicleType.Car, RegistrationNumber = "YYZ000", Color = "Red"  , Make = "Tesla"        , Model = "Model Y", NumberOfWheels =  4, ArrivalTime = new DateTime(2024, 04, 17, 09, 45, 00) },
+            new ParkedVehicle { VehicleType = VehicleType.Truck, RegistrationNumber = "TUV765", Color = "White", Make = "Mercedes-Benz", Model = "Econic" , NumberOfWheels = 10, ArrivalTime = new DateTime(2024, 04, 17, 10, 00, 00) },
         ];
 
         // Based on the Pluralsight course (ASP.Net Core 6 Fundamentals)
