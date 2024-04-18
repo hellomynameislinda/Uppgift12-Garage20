c7db8f948200acd417564d352daa6fdd80d4973c
jarethholt <jareth.holt@gmail.com>
2024-04-18 14:02:27 +0200

    feat: Add VehicleSummaryViewModel class
    Add the VehicleSummaryViewModel class containing select properties from the ParkedVehicle. Provides the ParkedTimeAmount function as a property. Include a method for generating a ViewModel from a vehicle instance.
    
    Todo: Replace the VehicleType (integer) property with the string name of the vehicle type.

---
+++ Uppgift12-Garage20/ViewModels/VehicleSummaryViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Uppgift12_Garage20.Helpers;
+using Uppgift12_Garage20.Models;
+
+namespace Uppgift12_Garage20.ViewModels
+{
+    public class VehicleSummaryViewModel
+    {
+        /* Currently using the integer VehicleType.
+         * This should be replaced with the string from the enum.
+         */
+        public int VehicleType { get; init; }
+        //public string VehicleTypeName { get; init; } = string.Empty;
+        public string RegistrationNumber { get; init; } = string.Empty;
+        public DateTime ArrivalTime { get; init; }
+
+        public TimeSpan ParkedTimeAmount =>
+            HelperFunctions.ParkedTimeAmount(ArrivalTime);
+
+        /// <summary>
+        /// Create a view model directly from a ParkedVehicle instance.
+        /// </summary>
+        /// <param name="vehicle">The ParkedVehicle to view.</param>
+        /// <returns>
+        /// A VehicleSummaryViewModel of the vehicle. This exposes the
+        /// VehicleType, RegistrationNumber, and ArrivalTime. It also exposes
+        /// the ParkedTimeAmount as a property.
+        /// </returns>
+        public static VehicleSummaryViewModel FromParkedVehicle(ParkedVehicle vehicle)
+            => new()
+            {
+                VehicleType = vehicle.VehicleType,
+                RegistrationNumber = vehicle.RegistrationNumber,
+                ArrivalTime = vehicle.ArrivalTime
+            };
+    }
+}
