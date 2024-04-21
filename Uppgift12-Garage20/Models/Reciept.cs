023a4f84419063746080c8885c5feca6c4cfcc81
jarethholt <jareth.holt@gmail.com>
2024-04-21 11:44:16 +0200

    feat: Add display attributes to Reciept model

--- Uppgift12-Garage20/Models/Reciept.cs
+++ Uppgift12-Garage20/Models/Reciept.cs
@@ -1,14 +1,21 @@
-﻿using Uppgift12_Garage20.Helpers;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Uppgift12_Garage20.Helpers;
 
 namespace Uppgift12_Garage20.Models
 {
+    [ModelMetadataType(typeof(VehicleModelMetaData))]
     public class Reciept
     {
         public int ParkedVehicleId { get; set; }
         public string RegistrationNumber { get; set; } = string.Empty;
         public DateTime ArrivalTime { get; set; }
 
+        [Display(Name = "Total Time Parked")]
         public TimeSpan TotalParkingTime { get; private set; }
+
+        [Display(Name = "Total Cost")]
+        [DisplayFormat(DataFormatString = "{0:N0}:-")]
         public decimal TotalCost { get; private set; }
 
         public Reciept(int parkedVehicleId, string registrationNumber, DateTime arrivalTime, decimal pricePerHour)
